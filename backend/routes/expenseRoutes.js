c5614e7236ab3cc96613656c302421debb0ebd81
Bhumika Tiwari <tbhumika95@gmail.com>
2026-06-04 03:44:07 +0530

    Created GET and DELETE APIs

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -15,4 +15,30 @@ router.post("/" , async(req , res)=> {
     }
 });
 
+router.get("/" , async(req, res) => {
+    try{
+        const expenses = await Expense.find();
+        res.status(200).json(expenses);
+    }
+    catch(error){
+        res.status(500).json({
+            message : error.message,
+        });
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedExpense = await Expense.findByIdAndDelete(
+            req.params.id
+        );
+
+        res.status(200).json(deletedExpense);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+});
+
 module.exports = router;
